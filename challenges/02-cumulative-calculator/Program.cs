1549edc2810e10263a6f9d066755b46635874b3f
Jessica <jessica.1702@hotmail.com>
2026-06-15 15:51:49 -0300

    feat: add challenge 02 - cumulative calculator

--- challenges/02-cumulative-calculator/Program.cs
+++ challenges/02-cumulative-calculator/Program.cs
@@ -1,2 +1,49 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Challenge 2 - Cumulative Calculator
+// Concepts: do-while + switch + double + type conversion
+// Each operation starts from the previous result
+// R to reset, S to quit
+
+using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        double resultado = 0;
+        string? operador = null;
+
+        do
+        {
+            Console.WriteLine($"Current result: {resultado}");
+            Console.Write("Operator (+, -, *, /, R to reset, S to quit): ");
+            operador = Console.ReadLine();
+
+            switch (operador)
+            {
+                case "+":
+                    Console.Write("Number: ");
+                    double numero = double.Parse(Console.ReadLine());
+                    resultado += numero;
+                    break;
+                case "-":
+                    Console.Write("Number: ");
+                    numero = double.Parse(Console.ReadLine());
+                    resultado -= numero;
+                    break;
+                case "*":
+                    Console.Write("Number: ");
+                    numero = double.Parse(Console.ReadLine());
+                    resultado *= numero;
+                    break;
+                case "/":
+                    Console.Write("Number: ");
+                    numero = double.Parse(Console.ReadLine());
+                    resultado /= numero;
+                    break;
+                case "R":
+                    resultado = 0;
+                    break;
+            }
+        } while (operador != "S");
+    }
+}
