08e46594f7295348f5d7e0964645b278ab4cc8cf
Jessica <jessica.1702@hotmail.com>
2026-06-15 15:53:40 -0300

    feat: add challenge 03 - odd and even numbers

--- challenges/03-odd-and-even/Program.cs
+++ challenges/03-odd-and-even/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Challenge 3 - Odd and Even numbers from 1 to 100
+// Concepts: for loop + % (modulus operator)
+// Display even numbers first, then odd numbers
+
+using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("Even:");
+        for (int i = 1; i <= 100; i++)
+        {
+            if (i % 2 == 0)
+                Console.Write(i + " ");
+        }
+
+        Console.WriteLine("\nOdd:");
+        for (int i = 1; i <= 100; i++)
+        {
+            if (i % 2 != 0)
+                Console.Write(i + " ");
+        }
+    }
+}
