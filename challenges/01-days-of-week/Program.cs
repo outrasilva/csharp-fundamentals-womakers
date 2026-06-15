a01966a0a659810d07b2c2ce48a25806c21dc213
Jessica <jessica.1702@hotmail.com>
2026-06-15 15:49:24 -0300

    feat: add challenge 01 - days of the week

--- challenges/01-days-of-week/Program.cs
+++ challenges/01-days-of-week/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Challenge 1 - Days of the Week
+// Concepts: string array + foreach
+// Store the days of the week in an array and display them one per line
+
+using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        string[] daysOfWeek = { "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday" };
+
+        foreach (string day in daysOfWeek)
+        {
+            Console.WriteLine(day);
+        }
+    }
+}
