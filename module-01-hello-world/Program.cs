636db7de6bed64286286b3b009a38961815b7046
Jessica <jessica.1702@hotmail.com>
2026-06-15 15:17:43 -0300

    feat: add module 01 - hello world and basic syntax

--- module-01-hello-world/Program.cs
+++ module-01-hello-world/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Module 1 - Write your first C# code
+// Concepts: Console.WriteLine, Console.Write, string literals, syntax
+
+// Print a message to the console
+Console.WriteLine("Hello World!");
+
+// Difference between WriteLine (breaks line) and Write (stays on same line)
+Console.Write("Congratulations! ");
+Console.Write("You wrote your first lines of code.");
